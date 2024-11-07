7dc06a1df7681d5b80cd112f6fc4fe7278bc2b91
aoirint <aoirint@gmail.com>
2024-11-07 23:14:01 +0900

    meteor

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool CheckMeteorShowerChance(int seed, int time) {
+  var meteorShowerChance = 7;
+  var random = new Random(seed + 28);
+
+  var isMeteorShower = random.Next(0, 1000) < meteorShowerChance;
+  if (! isMeteorShower) {
+    return false;
+  }
+
+  var meteorShowerAtTime = random.Next(5, 80);
+  return meteorShowerAtTime == time;
+}
+
+int seedCount = 0;
+
+while (true) {
+  var seed = new Random().Next(1, 100000001);
+
+  if (CheckMeteorShowerChance(seed, 5)) {
+    Console.WriteLine(seed);
+
+    seedCount += 1;
+    if (seedCount >= 100) {
+      break;
+    }
+  }
+}

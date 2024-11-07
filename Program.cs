67e3aea0a01f338b46d281c439a16788de6ac06d
aoirint <aoirint@gmail.com>
2024-11-07 23:23:33 +0900

    split check func

--- Program.cs
+++ Program.cs
@@ -1,14 +1,15 @@
-﻿bool CheckMeteorShowerChance(int seed, int time) {
+﻿bool CheckMeteorShowerChance(int seed) {
   var meteorShowerChance = 7;
   var random = new Random(seed + 28);
 
-  var isMeteorShower = random.Next(0, 1000) < meteorShowerChance;
-  if (! isMeteorShower) {
-    return false;
-  }
+  return random.Next(0, 1000) < meteorShowerChance;
+}
 
-  var meteorShowerAtTime = random.Next(5, 80);
-  return meteorShowerAtTime == time;
+bool CheckMeteorShowerTime(int seed, int time) {
+  var random = new Random(seed + 28);
+  random.Next(0, 1000);
+
+  return random.Next(5, 80) == time;
 }
 
 int seedCount = 0;
@@ -16,12 +17,18 @@ int seedCount = 0;
 while (true) {
   var seed = new Random().Next(1, 100000001);
 
-  if (CheckMeteorShowerChance(seed, 5)) {
-    Console.WriteLine(seed);
+  if (! CheckMeteorShowerChance(seed)) {
+    continue;
+  }
 
-    seedCount += 1;
-    if (seedCount >= 100) {
-      break;
-    }
+  if (! CheckMeteorShowerTime(seed, 5)) {
+    continue;
+  }
+
+  Console.WriteLine(seed);
+
+  seedCount += 1;
+  if (seedCount >= 100) {
+    break;
   }
 }
